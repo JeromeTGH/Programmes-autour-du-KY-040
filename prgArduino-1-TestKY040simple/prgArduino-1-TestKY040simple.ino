9a347aa0ba70a56318f3e261a7f1c4cb1afe6ab3
JeromeTGH <jerome.tomski@gmx.fr>
2023-05-01 16:31:05 +0200

    Écriture de la structure de base de l'exemple 1 arduino

--- prgArduino-1-TestKY040simple/prgArduino-1-TestKY040simple.ino
+++ prgArduino-1-TestKY040simple/prgArduino-1-TestKY040simple.ino
@@ -29,15 +29,33 @@
 #define pinArduinoRaccordementSignalCLKsoitA    4           // La pin D4 de l'Arduino recevra la ligne CLK du module KY-040 (soit la ligne "A" de l'encodeur)
 
 // Variables
-
-
+int etatPrecedentLigneSW        = HIGH;    // Cette variable nous permettra de stocker le dernier état de la ligne SW lue, afin de le comparer à l'actuel
+int etatPrecedentLigneCLKsoitA  = HIGH;    // Cette variable nous permettra de stocker le dernier état de la ligne CLK lue, afin de le comparer à l'actuel
+                                                    // Nota : j'ai mis HIGH par défaut, car les lignaux du KY-040, telles que câblées ici avec ce module,
+                                                    //        sont actives à l'état bas, donc, au repos à l'état haut (d'où le "HIGH")
 
 // ========================
 // Initialisation programme
 // ========================
 void setup() {
 
- 
+    // Initialisation de la liaison série (arduino nano <-> PC)
+    Serial.begin(9600);
+    Serial.println(F("========================================================================="));
+    Serial.println(F("Exemple 1 (programme de test Arduino Nano <-> module KY-040, scrutant en "));
+    Serial.println(F("           permanence certains changement d'états de lignes de l'encodeur"));
+    Serial.println(F("           KY-040, avec affichage des informations sur le moniteur série)"));
+    Serial.println(F("========================================================================="));
+    Serial.println("");
+
+    // Configuration des pins de notre Arduino Nano en "entrées", car elles recevront les signaux du KY-040
+    pinMode(pinArduinoRaccordementSignalSW, INPUT);
+    pinMode(pinArduinoRaccordementSignalDTsoitB, INPUT);
+    pinMode(pinArduinoRaccordementSignalCLKsoitA, INPUT);
+
+    // Petite pause pour laisser se stabiliser les signaux, avant d'attaquer la boucle loop
+    delay(200);
+
 }
 
 
@@ -46,5 +64,18 @@ void setup() {
 // =================
 void loop() {
 
+    // Lecture des signaux du KY-040 arrivant sur l'arduino
+    int etatActuelDeLaLigneSW       = digitalRead(pinArduinoRaccordementSignalSW);
+    int etatActuelDeLaLigneSCKsoitA = digitalRead(pinArduinoRaccordementSignalCLKsoitA);
+
+    // On regarde si le bouton SW a changé d'état
+    if(etatActuelDeLaLigneSW != etatPrecedentLigneSW) {
+        
+    }
+
+    // On regarde si le bouton CLK (soit A) a changé d'état
+    if(etatActuelDeLaLigneSCKsoitA != etatPrecedentLigneCLKsoitA) {
+        
+    }
 
 }
